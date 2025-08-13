17cae896563b6d341059a84205ea7ce5fca57cb7
Nasri-ahmed <info9345@gmail.com>
2025-08-13 20:20:29 +0100

    Implement JSP view for Hopper's Receipt with controller data binding

---
+++ Hopper/src/main/java/com/javastack/spring/hoper/HopperApplication.java
@@ -0,0 +1,13 @@
+package com.javastack.spring.hoper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopperApplication.class, args);
+	}
+
+}
